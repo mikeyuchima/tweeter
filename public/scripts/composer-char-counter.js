bd35181fa5bc47b4c370a1da2bbd735405c876e1
Michael Surya <tomichaels@gmail.com>
2018-07-17 20:49:23 +0000

    added character count functionality

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    console.log('ready');
+    $('textarea').keyup(function() {
+      var length = $(this).val().length
+      $('.counter').text(140 - length)
+      if (length > 140) {
+        $('.counter').addClass("error");
+      } else {
+        $('.counter').removeClass("error");
+      }
+      
+    })
+  });
