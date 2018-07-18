05b580ac0c1043cbe4be33cd3cbb8bdabd180a43
Michael Surya <tomichaels@gmail.com>
2018-07-18 22:30:51 +0000

    added tweet functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,54 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
- // Test / driver code (temporary). Eventually will get this from the server.
- const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
-
 $(document).ready(function() {
 function createTweetElement(data) {
     let username = data.user.name;
@@ -62,7 +14,7 @@ function createTweetElement(data) {
     let time_now = Date.now();
     let time_lapsed = calculateTime(time, time_now);
 
-    $('#tweets-container').append(`
+    $('#tweets-container').prepend(`
         <article class='tweet'>
         <header>
           <div class='topheader'>
@@ -83,27 +35,51 @@ function createTweetElement(data) {
     `)
 }
 
+  function calculateTime(then, now) {
+    return (Math.floor((now - then) / (1000 * 60 * 60 * 24)));
+}
+
 function renderTweets(tweets) {
     tweets.forEach(element => {
         createTweetElement(element);
     });
 }
 
-  function calculateTime(then, now) {
-    return (Math.floor((now - then) / (1000 * 60 * 60 * 24)));
-}
-  renderTweets(data)
-});
-
 $(function loadTweets() {
+    event.preventDefault()
+    let arr = $.getJSON('/tweets');
+    $( window ).load(function () {
+      $.ajax('/tweets', { method: 'GET' })
+      .then(function () {
+          console.log(arr.responseJSON);
+        renderTweets(arr.responseJSON);
+      });
+    });
+  });
+
+
+  $(function postTweets() {
     $('form').on('submit', function () {
     event.preventDefault()
     var query = $( "form" ).serialize();
     console.log( $( this ).serialize() );
+    if (query.length > 145) {
+        console.log(query.length)
+        return alert('Invalid Input');
+    }
       console.log('Button clicked, performing ajax call...');
       $.ajax('/tweets', { data: query,  method: 'POST' })
-      .then(function () {
-        console.log('Success: ', query);
+
+      .then(function (data, status) {
+          console.log(status);
+        console.log('Success: ', data);
+        createTweetElement(data);
+        return data;
+      }) 
+      .fail(function (data, status) {
+        console.log(status);
+        alert('Invalid Input')
       });
     });
   });
+});
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -39,6 +39,7 @@ module.exports = function(DataHelpers) {
         res.status(201).send();
       }
     });
+    res.json(tweet);
   });
 
   return tweetsRoutes;
